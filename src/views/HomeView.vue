7d1a9a3a93dcbc10cb715236b6a3e8accd9e6d7f
feneme7370 <marascofederico95@gmail.com>
2024-10-26 12:19:14 -0300

    carpeta modificada final

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,7 @@
 <template>
 <h2>DigitalCards</h2>
 
-<router-link :to="{name : 'aaaa_c_ninayaitana'}" class="">
+<router-link :to="{name : 'aaaa-c-ninayaitana'}" class="">
     Nina y Aitana.
   </router-link>
 
